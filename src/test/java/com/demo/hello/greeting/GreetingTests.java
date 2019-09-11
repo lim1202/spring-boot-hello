01f87eaddcd292073dcaad9104b33099a0d73861
lim <lim1202@gmail.com>
2019-09-11 16:08:05 +0800

    Remove unused comments.

--- src/test/java/com/demo/hello/greeting/GreetingTests.java
+++ src/test/java/com/demo/hello/greeting/GreetingTests.java
@@ -15,9 +15,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-/**
- * Test class for the {@link VetController}
- */
 @RunWith(SpringRunner.class)
 @WebMvcTest(GreetingController.class)
 public class GreetingTests {
